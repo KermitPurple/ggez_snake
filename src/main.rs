4aeeb2bd945c0d95ad6ab20bdc17e813e0c95a60
Shane McDonough <shanemcdman@gmail.com>
2021-08-06 22:23:28 -0700

    Write more for snake functions

--- src/main.rs
+++ src/main.rs
@@ -17,17 +17,43 @@ use ggez::{
     },
 };
 use winit::dpi::PhysicalPosition;
+use std::collections::VecDeque;
 
 type Point = mint::Point2<f32>;
 
+#[derive(Copy, Clone)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+impl Direction {
+    fn opposite(&self) -> Direction {
+        use Direction::*;
+        match self {
+            Up => Down,
+            Down => Up,
+            Left => Right,
+            Right => Left,
+        }
+    }
+}
+
 struct Game {
     conf: GameConf,
+    snake: Snake,
 }
 
 impl Game {
     fn new(_ctx: &mut Context, conf: GameConf) -> Self {
         Self {
             conf,
+            snake: Snake::new(new_point(
+                (conf.grid_size.x / 2.).floor(),
+                (conf.grid_size.y / 2.).floor(),
+            )),
         }
     }
 
@@ -59,20 +85,31 @@ impl Game {
         );
         graphics::draw(ctx, &square, DrawParam::default().dest(point))
     }
+
+    fn draw_snake(&mut self, ctx: &mut Context) -> GameResult {
+        self.draw_square(ctx, self.snake.head, Color::from_rgb(119, 199, 120))?;
+        for tail in self.snake.tail.clone() {
+            self.draw_square(ctx, tail, Color::from_rgb(119, 177, 120))?;
+        }
+        Ok(())
+    }
 }
 
 impl EventHandler<GameError> for Game {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
+        self.snake.update(self.conf.grid_size)?;
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, Color::BLACK);
         self.draw_grid(ctx)?;
+        self.draw_snake(ctx)?;
         graphics::present(ctx)
     }
 }
 
+#[derive(Copy, Clone)]
 struct GameConf {
     window_size: Point,
     grid_size: Point,
@@ -90,17 +127,57 @@ impl GameConf {
 
 }
 
-fn center_rect(w: f32, h: f32) -> Rect {
-    Rect {
-        x: -w / 2.,
-        y: -h / 2.,
-        w,
-        h,
-    }
+struct Snake {
+    head: Point,
+    tail: VecDeque<Point>,
+    length_to_add: i32,
+    direction: Direction,
+    alive: bool,
 }
 
-fn center_square(size: f32) -> Rect {
-    center_rect(size, size)
+impl Snake {
+    fn new(point: Point) -> Self {
+        Self {
+            head: point,
+            tail: VecDeque::new(),
+            length_to_add: 3,
+            direction: Direction::Up,
+            alive: true,
+        }
+    }
+
+    fn update_tail(&mut self) -> GameResult {
+        self.tail.push_front(self.head);
+        if self.length_to_add <= 0 {
+            let _ = self.tail.pop_back();
+        } else {
+            self.length_to_add -= 1;
+        }
+        Ok(())
+    }
+
+    fn update(&mut self, grid_size: Point) -> GameResult {
+        if !self.alive {
+            return Ok(());
+        }
+        let new_head = match self.direction {
+            Direction::Up => new_point(self.head.x, self.head.y - 1.),
+            Direction::Down => new_point(self.head.x, self.head.y + 1.),
+            Direction::Left => new_point(self.head.x - 1., self.head.y),
+            Direction::Right => new_point(self.head.x + 1., self.head.y),
+        };
+        if new_head.x < 0. ||
+            new_head.y < 0. ||
+            new_head.x >= grid_size.x ||
+            new_head.y >= grid_size.y ||
+            self.tail.contains(&new_head) {
+            self.alive = false;
+        } else {
+            self.update_tail()?;
+            self.head = new_head;
+        }
+        Ok(())
+    }
 }
 
 fn new_point(x: f32, y: f32) -> Point {
@@ -125,5 +202,5 @@ fn main() -> GameResult{
         .build()?;
     let game = Game::new(&mut ctx, conf);
     graphics::set_window_position(&ctx, PhysicalPosition::new(20, 20))?;
-    event::run(ctx, event_loop, game);
+    event::run(ctx, event_loop, game)
 }
